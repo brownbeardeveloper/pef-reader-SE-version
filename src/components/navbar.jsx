a682f361e58bcd0e3febc82ab3705edab15c9ce4
Björn Bergström <bjorn.bergstrom@mtm.se>
2024-05-06 15:56:32 +0200

    Added hidden links in header

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,11 +1,20 @@
+import { useState } from "react"
+import { TabIndex } from "../data/tab-index"
 import CookieBanner from "./cookie-banner"
 
 export default function Navbar({ showCookieBanner, setCookiePermission }) {
+  const [showShortcutLinks, setShowShortcutLinks] = useState(false)
 
   return (
     <header>
+      <div
+        className={`absolute top-0 left-0 m-10 px-10 py-2 bg-black text-white text-2xl border border-purple-500 ${showShortcutLinks ? 'block' : 'hidden'}`}
+        onFocus={() => setShowShortcutLinks(true)}>
+        <a tabIndex={TabIndex.HIDDEN_LINK_MAIN} href="#MainContentArea">test</a>
+      </div>
+
       {showCookieBanner && <CookieBanner setCookiePermission={setCookiePermission} />}
-    
+
       <div className="flex justify-between items-center px-4 py-1 mx-auto bg-gray-200">
         <h1 className="text-lg font-bold">Digital punktläsare</h1>
       </div>
--- src/data/tab-index.js
+++ src/data/tab-index.js
@@ -1,5 +1,6 @@
 export const TabIndex = {
-    UPLOAD_FILE: '1',
+    HIDDEN_LINK_MAIN: '1',
+    UPLOAD_FILE: '2',
     RADIO_FLOW_TEXT: '2', // not in use
     RADIO_BY_PAGE_TEXT: '3' // not in use
 }
