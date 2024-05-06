fef6c79d137beeca6e8ff62dbb481e388210916f
Björn Bergström <bjorn.bergstrom@mtm.se>
2024-05-06 11:24:49 +0200

    React Router DOM added

--- src/App.jsx
+++ src/App.jsx
@@ -2,31 +2,36 @@ import Navbar from './components/navbar.jsx'
 import Footer from './components/footer.jsx'
 import Main from './pages/main.jsx'
 import CookieBanner from './components/cookie-banner.jsx';
+import InstructionPage from './pages/instruction.jsx';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { getAllowCookie, setAllowCookie } from './functions/cookieManager.js';
 import { useState, useEffect } from 'react';
 
 export default function App() {
-    const [cookiePermission, setCookiePermission] = useState(getAllowCookie());
-    const [showCookieBanner, setShowCookieBanner] = useState()
+  const [cookiePermission, setCookiePermission] = useState(getAllowCookie());
+  const [showCookieBanner, setShowCookieBanner] = useState()
 
-    useEffect(() => {
-      if (cookiePermission === "allowed") {
-        setAllowCookie(true)
-        setShowCookieBanner(false);
-      } else if (cookiePermission === "denied") {
-        setAllowCookie(false)
-        setShowCookieBanner(false);
-      } else {
-        setShowCookieBanner(true); // if there's no cookies
-      }
-    }, [cookiePermission]);
+  useEffect(() => {
+    if (cookiePermission === "allowed") {
+      setAllowCookie(true)
+      setShowCookieBanner(false);
+    } else if (cookiePermission === "denied") {
+      setAllowCookie(false)
+      setShowCookieBanner(false);
+    } else {
+      setShowCookieBanner(true); // if there's no cookies
+    }
+  }, [cookiePermission]);
 
   return (
-    <>
+    <Router>
       {showCookieBanner && <CookieBanner setCookiePermission={setCookiePermission} />}
       <Navbar />
-      <Main cookiePermission={cookiePermission} />
+      <Routes>
+        <Route path="/" element={<Main cookiePermission={cookiePermission} />} />
+        <Route path="/instruktion" element={<InstructionPage />} />
+      </Routes>
       <Footer />
-    </>
+    </Router>
   );
 }
