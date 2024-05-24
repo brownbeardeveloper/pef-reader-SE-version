02dbffeb57899de4ea97fea1d65e5225e063347d
Björn Bergström <bjorn.bergstrom@mtm.se>
2024-05-24 10:16:42 +0200

    Added new comments into the playVoice functions

--- src/functions/playVoice.js
+++ src/functions/playVoice.js
@@ -1,12 +1,12 @@
 import positionSavedMp3 from '../data/voices/position-saved-voice.mp3';
 import countineReadingMp3 from '../data/voices/countine-reading-voice.mp3';
 
-export function PositionSavedVoice() {
+export function PositionSavedVoice() { // not in use
         const audio = new Audio(positionSavedMp3);
         audio.play();
 }
 
-export function CountineReadingVoice() {
+export function CountineReadingVoice() { // not in use
         const audio = new Audio(countineReadingMp3);
         audio.play();
 }
